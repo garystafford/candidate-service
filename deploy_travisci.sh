885beefb8d4d56df33fe44e416175db5959a3c87
Gary Stafford <garystafford@rochester.rr.com>
2017-01-25 21:08:02 -0500

    Docker build now happens on Travis CI not Docker Hub

--- deploy_travisci.sh
+++ deploy_travisci.sh
@@ -27,6 +27,6 @@ docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker build -t garystafford/candidate-service:latest .
 docker push garystafford/candidate-service:latest
 
-IMAGE_TAG="v0.2.${TRAVIS_BUILD_NUMBER}"
+IMAGE_TAG="0.2.${TRAVIS_BUILD_NUMBER}"
 docker tag garystafford/candidate-service:latest garystafford/candidate-service:${IMAGE_TAG}
 docker push garystafford/candidate-service:${IMAGE_TAG}
