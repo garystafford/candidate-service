2886315b3dc76565325e7b1a789c6da06fd4c25f
Gary A. Stafford <garystafford@rochester.rr.com>
2017-05-13 14:12:15 -0400

    Adding simple async messaging

--- scripts/post_candidates.sh
+++ scripts/post_candidates.sh
@@ -61,6 +61,8 @@ done
 TIME2=$(date +%s)
 
 TIME3=`expr ${TIME2} - ${TIME1}`
-
+TESTS = `expr ${TEST_CYCLES} * 6`
+echo ""
+echo "It took ${TIME3} seconds for ${TEST_CYCLES} test cycles, or ${TESTS}"
 echo ""
 echo "Script completed..."
