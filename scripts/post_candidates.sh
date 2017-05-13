174d6c6bedaad19fc3d43814e0999ee1023cc3d5
Gary A. Stafford <garystafford@rochester.rr.com>
2017-05-12 22:39:19 -0400

    Adding simple async messaging

--- scripts/post_candidates.sh
+++ scripts/post_candidates.sh
@@ -1,46 +1,66 @@
 #!/bin/sh
 
-# Post 2016 Presidential Election candidates
+# Post new candidates
 # Requires HTTPie
 # Requires all services are running
 
 set -e
 
-http POST http://localhost:8097/candidate/candidates \
-  firstName='Donald' \
-  lastName='Trump' \
-  politicalParty='Republican Party' \
-  election='2016 Presidential Election'
+HOST=${1:-localhost}
+TEST_CYCLES=${2:-25}
+URL="http://${HOST}:8097"
+ELECTION="2016 Presidential Election"
 
-http POST http://localhost:8097/candidate/candidates \
-  firstName='Chris' \
-  lastName='Keniston' \
-  politicalParty='Veterans Party' \
-  election='2016 Presidential Election'
+echo "POSTing new candidates..."
 
-http POST http://localhost:8097/candidate/candidates \
-  firstName='Jill' \
-  lastName='Stein' \
-  politicalParty='Green Party' \
-  election='2016 Presidential Election'
+TIME1=$(date +%s)
+for i in $(seq ${TEST_CYCLES})
+do
+  http POST ${URL}/candidate/candidates \
+    firstName='Donald' \
+    lastName='Trump' \
+    politicalParty='Republican Party' \
+    election="${ELECTION}" \
+    --headers > /dev/null
 
-http POST http://localhost:8097/candidate/candidates \
-  firstName='Gary' \
-  lastName='Johnson' \
-  politicalParty='Libertarian Party' \
-  election='2016 Presidential Election'
+  http POST ${URL}/candidate/candidates \
+    firstName='Chris' \
+    lastName='Keniston' \
+    politicalParty='Veterans Party' \
+    election="${ELECTION}" \
+    --headers > /dev/null
 
-http POST http://localhost:8097/candidate/candidates \
-  firstName='Darrell' \
-  lastName='Castle' \
-  politicalParty='Constitution Party' \
-  election='2016 Presidential Election'
+  http POST ${URL}/candidate/candidates \
+    firstName='Jill' \
+    lastName='Stein' \
+    politicalParty='Green Party' \
+    election="${ELECTION}" \
+    --headers > /dev/null
 
-http POST http://localhost:8097/candidate/candidates \
-  firstName='Hillary' \
-  lastName='Clinton' \
-  politicalParty='Democratic Party' \
-  election='2016 Presidential Election'
+  http POST ${URL}/candidate/candidates \
+    firstName='Gary' \
+    lastName='Johnson' \
+    politicalParty='Libertarian Party' \
+    election="${ELECTION}" \
+    --headers > /dev/null
+
+  http POST ${URL}/candidate/candidates \
+    firstName='Darrell' \
+    lastName='Castle' \
+    politicalParty='Constitution Party' \
+    election="${ELECTION}" \
+    --headers > /dev/null
+
+  http POST ${URL}/candidate/candidates \
+    firstName='Hillary' \
+    lastName='Clinton' \
+    politicalParty='Democratic Party' \
+    election="${ELECTION}" \
+    --headers > /dev/null
+done
+TIME2=$(date +%s)
+
+TIME3=`expr ${TIME2} - ${TIME1}`
 
 echo ""
 echo "Script completed..."
