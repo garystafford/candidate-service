9bdf2671d4e7c83098c037ec32db42fa23cfcb2c
Gary A. Stafford <garystafford@rochester.rr.com>
2017-05-14 20:12:07 -0400

    Adding election service

--- src/main/java/com/voterapi/candidate/service/ElectionService.java
+++ src/main/java/com/voterapi/candidate/service/ElectionService.java
@@ -9,12 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-/**
- * Created by garystafford on 5/14/17.
- */
+@Service
 public class ElectionService {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
